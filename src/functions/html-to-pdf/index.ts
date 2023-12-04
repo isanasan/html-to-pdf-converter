bdbfddf90999301269815b8ec69a63b311cfedfd
isanasan <yfcgpsebp@gmail.com>
2023-12-04 11:06:27 +0900

    fix: layer ARNをハードコート

--- src/functions/html-to-pdf/index.ts
+++ src/functions/html-to-pdf/index.ts
@@ -3,7 +3,7 @@ import { handlerPath } from "@libs/handler-resolver";
 export default {
 	handler: `${handlerPath(__dirname)}/handler.main`,
 	layers: [
-		"arn:aws:lambda:::layer:${sls:stage}-chromium:1"
+		"arn:aws:lambda:ap-northeast-1:180499926846:layer:dev-chromium:1"
 	],
 	timeout: 15,
 	memorySize: 2048,
